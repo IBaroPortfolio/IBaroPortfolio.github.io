f6e56d0925882a4d40d17667aa52420eb1c872cd
IBaro <127464637+IBaroPortfolio@users.noreply.github.com>
2025-03-05 21:39:38 -0500

    Add files via upload

---
+++ public/translations.js
@@ -0,0 +1,249 @@
+export default {
+    en: {
+        header:{
+            home: "Home",
+            about: "About",
+            projects: "Projects",
+            contact: "Contact me",
+        },
+
+        home:{
+            name: "Hi, I'm"
+        },
+
+        about:{
+            title: "About me",
+            text1: "I’m an UI designer and illustrator with a great sense of responsibility and organization, which allows me to manage design projects with efficiency and creativity.",
+            text2: "I focus on providing aesthetic solutions that enhance the user experience with a high level of detail."
+        },
+
+        skills:{
+            title: "Skills",
+            skills: {
+                title: "Skills",
+                ui: "UI Design",
+                ux: "UX Design",
+                brand: "Brand identity",
+                digital: "Digital illustration",
+                traditional: "Traditional illustration",
+            },
+
+            softskills:{
+                title: "Soft skills",
+                adapt: "Adaptability",
+                create: "Creativity",
+                strategy: "Strategic thinking",
+                time: "Time management",
+                team: "Teamwork"
+            },
+
+            software:{
+                adobe: "Adobe Suite"
+            },
+
+            language:{
+                title: "Languages",
+                es: "Spanish",
+                esText: "Native",
+                en: "English"
+            }
+        },
+
+        btns:{
+            more: "See more",
+            cv: "Check my CV"
+        },
+
+        cvLinks: "./public/assets/CV-en.pdf",
+
+        experience:{
+            title: "Experience",
+            work:{
+                title: "Work Experience",
+                group1:{
+                    date: "September 2024 - Present day",
+                    job: "Illustrator",
+                    place: "Trans-pasando Fronteras Magazine, Cali"
+                },
+                group2:{
+                    date: "August 2024 - Present day",
+                    job: "Monitor"
+                },
+                group3:{
+                    date: "February 2024 - July 2024",
+                    job: "UI designer and frontend developer"
+                }
+            },
+
+            education:{
+                title: "Education",
+                group1:{
+                    title: "Interactive Media Design",
+                    achieve: "Achievements",
+                    li1: "Academic Excellence Scholarship - Silver category",
+                    li2: "Honor Roll 2021-2 - Category 1",
+                    li3: "Pixel Media award– 1st place for Audiovisual production"
+                },
+                group2:{
+                    title: "High School"
+                }
+            },
+
+            extra:{
+                title: "Extracurriculars",
+                group1:{
+                    date: "July 2024 - Present day",
+                    job: "Design committee leader",
+                    place: "Güinev student group"
+                }
+            }
+        },
+
+        projects: "Projects",
+
+        contact: {
+            title: "Contact me",
+            follow: "Follow me in"
+        },
+
+        footer: {
+            develop: "Developed by Isabella Barona 2024",
+            github: "Deployed in Github Pages"
+        },
+
+        filters: {
+            all: "All",
+            ui: "UI Design",
+            ux: "UX Design",
+            code: "Code",
+            graphic: "Graphic Design",
+            brand: "Brand Design",
+            illustration: "Illustration",
+            animation: "Animation"
+        }
+    },
+
+    es: {
+        header: {            
+            home: "Inicio",
+            about: "Acerca de",
+            projects: "Proyectos",
+            contact: "Contáctame",
+        },
+
+        home:{
+            name: "Hola, soy"
+        },
+
+        about:{
+            title: "Sobre mi",
+            text1: "Soy una diseñadora UI e ilustradora con un gran sentido de responsabilidad y organización, lo que me permite manejar proyectos de diseño con eficiencia y creatividad.",
+            text2: "Me enfoco en brindar soluciones estéticas con gran nivel de detalle que mejoren la experiencia de los usuarios."
+        },
+
+        skills:{
+            title: "Habilidades",
+            skills: {
+                title: "Habilidades",
+                ui: "Diseño UI",
+                ux: "Diseño UX",
+                brand: "Identidad de marca",
+                digital: "Ilustración digital",
+                traditional: "Ilustración tradicional",
+            },
+
+            softskills:{
+                title: "Habilidades blandas",
+                adapt: "Adaptabilidad",
+                create: "Creatividad",
+                strategy: "Pensamiento estratégico",
+                time: "Manejo del tiempo",
+                team: "Trabajo en equipo"
+            },
+
+            software:{
+                adobe: "Suite de Adobe"
+            },
+
+            language:{
+                title: "Idiomas",
+                es: "Español",
+                esText: "Nativo",
+                en: "Inglés"
+            }
+        },
+
+        btns:{
+            more: "Ver más",
+            cv: "Revisa mi CV"
+        },
+
+        cvLinks: "./public/assets/CV-es.pdf",
+
+        experience:{
+            title: "Experiencia",
+            work:{
+                title: "Experiencia laboral",
+                group1:{
+                    date: "Septiembre 2024 - Actualidad",
+                    job: "Illustradora",
+                    place: "Revista Trans-pasando Fronteras, Cali"
+                },
+                group2:{
+                    date: "Agosto 2024 - Actualidad",
+                    job: "Monitora"
+                },
+                group3:{
+                    date: "Febrero 2024 - Julio 2024",
+                    job: "Diseñadora UI y desarrolladora frontend"
+                }
+            },
+
+            education:{
+                title: "Educación",
+                group1:{
+                    title: "Diseño de Medios Interactivos",
+                    achieve: "Logros",
+                    li1: "Beca de Excelencia Académica - Categoría Plata",
+                    li2: "Cuadro de Honor 2021-2 - Categoría 1",
+                    li3: "Premio Pixel Media – 1er lugar en Producción Audiovisual"
+                },
+                group2:{
+                    title: "Bachillerato"
+                }
+            },
+
+            extra:{
+                title: "Extracurriculares",
+                group1:{
+                    date: "Julio 2024 - Actualidad",
+                    job: "Líder del comité de diseño",
+                    place: "Grupo estudiantil Güinev"
+                }
+            }
+        },
+
+        projects: "Proyectos",
+
+        contact: {
+            title: "Contáctame",
+            follow: "Sígueme en"
+        },
+
+        footer: {
+            develop: "Desarrollado por Isabella Barona 2024",
+            github: "Implementado en Github Pages"
+        },
+
+        filters: {
+            all: "Todo",
+            ui: "Diseño UI",
+            ux: "Diseño UX",
+            code: "Código",
+            graphic: "Diseño Gráfico",
+            brand: "Diseño de marca",
+            illustration: "Illustración",
+            animation: "Animación"
+        }
+    }
+};
